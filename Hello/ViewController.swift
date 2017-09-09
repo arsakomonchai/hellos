69ca4368bd870d1df2e8787e9506036d6e054ef2
keng-volunteerr <arsa_ko@hotmail.com>
2017-09-09 11:43:58 +0700

    Replace label ok

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    //Imlicit  ประกาศตัวแปร
+    var nameString = ""
+    
+    
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    
+    
+    
+    @IBAction func addButton(_ sender: Any) {
+        nameString = nameTextField.text!
+        
+        print("You type ==>\(nameString)")  //การสั่งพิมพ์
+        nameLabel.text = nameString
+        
+    }//AddButton
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }   //Main Function  เริ่มทำงานในส่วนนี้ก่อน เสมอ
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }   //manage Memory
 
 
-}
+}   //Main Class
 
